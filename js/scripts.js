d76ef28ac13408ce91a98a38a2d3fb99fb92e3da
AlexisToranzo <toranzoalexis11@gmail.com>
2022-09-11 00:03:49 -0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+console.log("comenzando cargA")
+
+
+window.onload = function(){
+    console.log("cargado")
+    document.getElementById("preloadmessage").style.display = "none"
+    document.getElementById("preload").style.filter = "blur(0px)"
+
+    
+
+}
+
+
+function hobbie(){
+        let hobbies = ["jugar futbol","entrenar","meditar","escuchar musica"]
+        
+        
+        
+}
